010ca9d959852ccf2b6165766bbebd45940014a9
galakerman <akermang@gmail.com>
2018-04-17 23:52:14 +0300

    login.js moved , Login css moved

---
+++ apps/Customer/js/Login.js
@@ -0,0 +1,57 @@
+var showStatusMessages = false;
+var origRequestFields;
+
+$(function () {
+    origRequestFields = $('#requestFields').clone();
+    setDefaultValues();
+    initData();
+    validateRequiredFields();
+
+    $('input').on('input blur', function () {
+        setTimeout(validateRequiredFields, 200);
+    });
+});
+
+var defaultValues = {};
+defaultValues['MAIN_USERNAME_0'] = {
+    value: 'MIA',
+    type: 'text'
+};
+
+defaultValues['MAIN_PASSWORD_0'] = {
+    value: 'ORACLE',
+    type: 'password'
+};
+
+function showInputScreen() {
+    setDefaultValues();
+    refreshInputElements();
+
+    $("#requestPart").show();
+    $("#responsePart").hide();
+
+    clearOutputFields();
+}
+
+//handler for request sent
+function sendRequestHandler() {
+    document.getElementById("responsePart").style.display = 'none';
+    spinner().start("Executing..", "a");
+}
+
+//handler for response received
+function receiveResponseHandler(response, serviceName, status, populateFields) {
+    spinner().stop();
+
+    if (typeof (populateFields) === 'undefined' || populateFields) {
+        //show output params
+
+        document.getElementById("responsePart").style.display = '';
+        validateRequiredFields();
+    }
+}
+
+//init function
+$(function () {
+    removeDuplicatedFields();
+});
